bca43e07587bd90a28238e968e827d379fce4750
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-08-29 16:54:12 +0530

    Create ultra_Fast_Mathematician.java

---
+++ ultra_Fast_Mathematician.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ultra_Fast_Mathematician{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String b = scanner.next();
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0; i<a.length(); i++){
+            result.append(a.charAt(i)^b.charAt(i));
+        }
+        System.out.println(result);
+    }
+
+
+}
