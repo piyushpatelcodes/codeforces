0706cf8015f1d992d4a8e3b7c221ec35fc769707
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-08-29 12:53:36 +0530

    Create is_your_horseshoe_on_the_other_hoof.java

---
+++ is_your_horseshoe_on_the_other_hoof.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class a{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+  Set<Integer> set= new HashSet<>();
+      for(int i=0; i<4; i++){
+      set.add(sc.nextInt());
+      }
+  if(set.size() == 4){
+  System.out.println(0);
+    }
+  else{System.out.println(4 - set.size());
+  }
+  sc.close();
+  
+  }}
