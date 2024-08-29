7df3023f99cd9df1fca2429e5f894d4ecd696e32
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-08-29 16:09:43 +0530

    Create stones_on_the_Table.java

---
+++ stones_on_the_Table.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class stones_on_the_Table{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = scanner.next();
+        int count = 0;
+
+        for(int i=0; i < n -1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        
+        scanner.close();
+    }
+
+
+}
