98a8eec520d36f9175fe843e0c487f8239bcd0c7
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-08-31 00:10:10 +0530

    Create three_Brothers.java

---
+++ three_Brothers.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class three_Brothers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a= scanner.nextInt();
+        int b =  scanner.nextInt();
+
+
+        int sum = 1+2+3;
+        int curr = a+b;
+        System.out.println(sum - curr);
+        
+
+      
+
+
+
+    }
+}
+// Solution by piyush patel codes https://codeforces.com/profile/piyushpatelcodes   https://www.linkedin.com/in/piyushpatelcodes/
