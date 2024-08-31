3e3e617c80544930f8ad501b2253d9b20f7db253
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-08-31 15:16:01 +0530

    Create hexadecimal_Theorem.java

---
+++ hexadecimal_Theorem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class hexadecimal_Theorem {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+
+        System.out.println(n + " 0 0");
+
+
+      
+
+    }
+}
+// Solution by piyush patel codes https://codeforces.com/profile/piyushpatelcodes   https://www.linkedin.com/in/piyushpatelcodes/
