8f23273f1d3ef969518486bded30dcdf75fba734
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-09-03 01:10:53 +0530

    Create little_Elephant_and_Function.java

---
+++ little_Elephant_and_Function.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class little_Elephant_and_Function {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.print(n + " ");
+        
+        for (int i = 1; i < n; i++) {
+            System.out.print(i + " ");
+        }
+        
+        sc.close();
+    }
+}
+// Solution by piyush patel codes https://codeforces.com/profile/piyushpatelcodes   https://www.linkedin.com/in/piyushpatelcodes/
