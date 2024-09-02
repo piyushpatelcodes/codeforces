d91eee9830702574052ed9effaeb892a9892bbba
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2024-09-02 19:28:22 +0530

    Create dubstep.java

---
+++ dubstep.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class dubstep {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.next();
+
+        s=s.replace("WUB", " ");
+        System.out.println(s.trim());
+    }
+}
+// Solution by piyush patel codes https://codeforces.com/profile/piyushpatelcodes   https://www.linkedin.com/in/piyushpatelcodes/
