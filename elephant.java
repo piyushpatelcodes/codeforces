5792b02dab48cced72e92f12dd04e9ba7bcd0b5a
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2025-02-22 21:25:15 +0530

    Create elephant.java

---
+++ elephant.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class elephant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n / 5 + (n % 5 == 0 ? 0 : 1));
+       
+        // int ans=0;
+        // while(n>0){
+
+        //     if(n>=5){
+        //         n=n-5;
+        //         ans++;
+        //     }
+        //     else if(n >= 4){
+        //         n=n-4;
+        //         ans++;
+        //     }
+        //     else if(n >= 3){
+        //         n=n-3;
+        //         ans++;
+        //     }
+        //     else if(n >= 2){
+        //         n=n-2;
+        //         ans++;
+        //     }
+        //     else if(n >= 1){
+        //         n=n-1;
+        //         ans++;
+        //     }
+        // }
+
+        // System.out.println(ans);
+         
+    }
+}
+
+
+// Solution by piyush patel codes https://codeforces.com/profile/piyushpatelcodes   https://www.linkedin.com/in/piyushpatelcodes/
